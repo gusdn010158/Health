a8385117417c6cd298e64807d8a9f96cb1abac4e
gusdn010158 <gusdn010158@gmail.com>
2024-07-19 23:35:11 +0900

    refactor signup

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,21 +10,10 @@ export default function Login() {
   };
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const handleLogin = () => {
-    // 로그인 처리 로직을 구현합니다.
-    // 성공적으로 로그인되었다면 setIsLoggedIn(true)로 상태를 변경합니다.
-    setIsLoggedIn(true);
-  };
-
-  const handleLogout = () => {
-    // 로그아웃 처리 로직을 구현합니다.
-    // 로그아웃되었다면 setIsLoggedIn(false)로 상태를 변경합니다.
-    setIsLoggedIn(false);
-  };
   const buttonData = [
     { label: "PRODUCTS", path: "/Products" },
     { label: "COMMUNITY", path: "/Community" },
-    { label: "TODO LIST", path: "/Todos" },
+    { label: "PERSONAL", path: "/PERSONAL" },
   ];
   return (
     <div>
