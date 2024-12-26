41d3569db55faa5467d399ecaa7ed7e7d1818504
gusdn010158 <gusdn010158@gmail.com>
2024-12-26 20:49:11 +0900

    delete

--- src/components/Products/Productsmain.js
+++ src/components/Products/Productsmain.js
@@ -1,22 +1,50 @@
 import React from "react";
-import "./Productsmain.css";
+import img from "../img/showingpage.png";
 import advertisement from "../img/coad.jpg";
 import Productsmainbest from "./Productsmainbest";
+import styled from "styled-components";
 
 function Productsmain() {
   return (
-    <div className="Productsmain">
-      <div className="advertisement">
+    <Productsmai>
+      <Advertisement>
         <img src={advertisement} alt="advertisement" />
 
         <Productsmainbest apiEndpoint="products" big="Best" />
 
-        <div className="prototype" />
-
+        <Prototype />
         <Productsmainbest apiEndpoint="massage" big="New" />
-      </div>
-    </div>
+      </Advertisement>
+    </Productsmai>
   );
 }
 
 export default Productsmain;
+
+const Productsmai = styled.div`
+  padding-left: 15%;
+  background-color: #ffff;
+  width: 100%;
+  height: 2000px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Advertisement = styled.div`
+  background-color: #ffff;
+
+  > img {
+    width: 100%;
+    height: 400px;
+    background-size: cover;
+  }
+`;
+
+const Prototype = styled.div`
+  margin-top: 10px;
+  height: 500px;
+  width: 100%;
+  background-attachment: fixed;
+  background-size: cover;
+  background-image: url(${img});
+`;
