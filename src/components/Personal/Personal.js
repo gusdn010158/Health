e4af1384f7cc72d4b1eb1d6dea527f7adea727f4
gusdn010158 <gusdn010158@gmail.com>
2025-01-03 23:26:35 +0900

    rfgg

--- src/components/Personal/Personal.js
+++ src/components/Personal/Personal.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from "react";
-import "./Personal.css";
+import styled from "styled-components";
 import TodoInput from "./TodoInput";
 import TodoList from "./TodoList";
 import Chart from "./Chart";
@@ -8,6 +8,128 @@ import Calendar from "react-calendar";
 import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
 import TileContent from "./TileContent";
 import { Link } from "react-router-dom";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 100vh;
+`;
+
+const LeftSide = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Todos = styled.div`
+  border-radius: 1rem;
+  margin: 3rem 0 3rem 5rem;
+  box-shadow: 5px 5px 20px lightgray;
+
+  h1 {
+    background: rgb(203, 170, 254);
+    font-family: "oneFontTitle";
+    text-align: center;
+    padding: 25px 0;
+    font-size: 20px;
+    color: white;
+    border-radius: 10px 10px 50% 50%;
+  }
+`;
+
+const StyledLink = styled(Link)`
+  background: rgb(203, 170, 254);
+  padding: 25px;
+  border-radius: 25px;
+  text-decoration: none;
+  color: white;
+  font-size: 18px;
+  margin-left: 30px;
+`;
+
+const StyledCalendar = styled(Calendar)`
+  border-radius: 1rem;
+  margin: 2rem;
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0px 0px 20px lightgray;
+  height: 35rem;
+  width: 45rem;
+
+  .sundayTile {
+    color: red;
+  }
+
+  .saturdayTile {
+    color: blue;
+  }
+
+  .react-calendar__navigation button {
+    min-width: 100px;
+    font-size: 1.5rem;
+    margin-bottom: 2rem;
+  }
+
+  .currentMonthTile,
+  button.sundayTile,
+  button.saturdayTile {
+    padding: 5px;
+    padding-bottom: 1rem;
+  }
+
+  .react-calendar__navigation button:enabled:hover,
+  .react-calendar__navigation button:enabled:focus {
+    background-color: #f8f8fa;
+  }
+
+  abbr[title] {
+    text-decoration: none;
+  }
+
+  .react-calendar__year-view__months__month,
+  .react-calendar__decade-view__years__year,
+  .react-calendar__century-view__decades__decade {
+    color: black;
+  }
+
+  .react-calendar__tile--now {
+    background-color: rgb(255, 205, 213);
+  }
+
+  .react-calendar__navigation__label > span {
+    font-size: 20px;
+    font-weight: bold;
+  }
+
+  .react-calendar__month-view__weekdays {
+    margin-bottom: 1rem;
+    text-align: center;
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 13px;
+  }
+
+  .react-calendar__month-view__days__day--neighboringMonth {
+    color: #b8b6b6;
+  }
+
+  .react-calendar__tile {
+    border: 0.5px solid #cccccc;
+    text-align: center;
+  }
+
+  .react-calendar__tile:enabled:hover {
+    background-color: #f8f8fa;
+  }
+
+  .react-calendar__tile:enabled:focus {
+    background-color: rgb(203, 170, 254);
+  }
+`;
+
 const Personal = () => {
   const no = useRef(1);
 
@@ -56,19 +178,19 @@ const Personal = () => {
   return (
     <div>
       <Header />
-      <div className="personal_container">
-        <div className="personal_leftSide">
+      <Container>
+        <LeftSide>
           <span>
             <Chart />
           </span>
-          <div className="Todos">
+          <Todos>
             <h1>TODO LIST</h1>
             <TodoInput onAdd={onAdd} />
             <TodoList todos={todos} onDel={onDel} onToggle={onToggle} />
-          </div>
-        </div>
+          </Todos>
+        </LeftSide>
         <div>
-          <Calendar
+          <StyledCalendar
             className="calendar-parent"
             value={value}
             onChange={onChange}
@@ -82,12 +204,9 @@ const Personal = () => {
               <TileContent date={date} tileClassName={tileClassName} />
             )}
           />
-
-          <Link to="/Home" className="flinkbtn">
-            내 정보로
-          </Link>
+          <StyledLink to="/Home">내 정보로</StyledLink>
         </div>
-      </div>
+      </Container>
     </div>
   );
 };
--- src/components/Personal/TodoList.js
+++ src/components/Personal/TodoList.js
@@ -1,19 +1,54 @@
 import React from "react";
+import styled from "styled-components";
 import TodoItem from "./TodoItem";
-import "./TodoList.css";
+
+const StyledTodoList = styled.ul`
+  padding: 0;
+
+  li {
+    background-color: white;
+    padding: 15px 30px;
+    color: rgb(0, 0, 0);
+    font-weight: bold;
+    list-style: none;
+
+    span {
+      margin-right: 25px;
+      cursor: pointer;
+    }
+
+    &.on span {
+      color: black;
+    }
+
+    em {
+      cursor: pointer;
+    }
+
+    &.on em {
+      color: black;
+      text-decoration: line-through rgb(159, 127, 231);
+    }
+
+    button {
+      float: right;
+      color: rgb(175, 169, 169);
+    }
+  }
+`;
 
 const TodoList = ({ todos, onDel, onToggle }) => {
   return (
-    <ul className="TodoList">
-      {todos.map((todos) => (
+    <StyledTodoList>
+      {todos.map((todo) => (
         <TodoItem
-          key={todos.id}
-          todos={todos}
+          key={todo.id}
+          todos={todo}
           onDel={onDel}
           onToggle={onToggle}
         />
       ))}
-    </ul>
+    </StyledTodoList>
   );
 };
 
